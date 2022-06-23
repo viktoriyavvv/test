411424414e97f9f283ffd5b6f9013fcd5b32009d
viktorriyavvv <viktoriya@adamar.solutions>
2022-06-23 17:59:47 +0600

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="container static p-2">
-    <input
-      class="fixed bottom-5 z-10 opacity-0"
-      type="file"
-      id="files"
-      ref="files"
-      accept="image/*"
-      multiple
-      v-on:change="uploadImg()"
-    />
-    <span>
-      <img
-        src="/src/assets/DownloadPhoto.png"
-        alt="Download"
-        class="w-10 fixed bottom-3 left-40"
+  <div class="container relative mx-auto p-2">
+    <div class="flex justify-center">
+      <input
+        class="fixed bottom-5 z-10 opacity-0"
+        type="file"
+        id="files"
+        ref="files"
+        accept="image/*"
+        multiple
+        v-on:change="uploadImg()"
       />
-    </span>
+      <span>
+        <img
+          src="/src/assets/DownloadPhoto.png"
+          alt="Download"
+          class="w-10 fixed bottom-3 bg-slate-200 rounded-lg"
+        />
+      </span>
+    </div>
 
     <div>
       <div class="columns-3 gap-2">
